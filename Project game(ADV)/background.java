e93fbc7c6034f0030ead54d4b42e3233ad81b823
andidian899 <38680061+andidian899@users.noreply.github.com>
2018-04-24 17:04:01 +0800

    Phase 1

---
+++ Project game(ADV)/background.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class background here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class background extends scroll
+{
+    /**
+     * Act - do whatever the background wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }   
+    public void scroll()
+    {
+        if(getX()<-getImage().getWidth()/2)
+        {
+            setLocation(getWorld().getWidth()+508,getY());
+        }
+        setLocation(getX()-2,getY());
+    }
+}
---
+++ Project game(ADV)/scroll.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class scroll here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class scroll extends Actor
+{
+    /**
+     * Act - do whatever the scroll wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
