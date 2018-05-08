33c366c2758c50876c2170ec26c160f7c56474f4
Vinsen98 <38679732+Vinsen98@users.noreply.github.com>
2018-05-08 13:09:20 +0800

    Add files via upload

--- Project game(ADV)/enemy.java
+++ Project game(ADV)/enemy.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class scroll here.
+ * Write a description of class enemy here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class scroll extends Actor
+public class enemy extends scroll
 {
     /**
-     * Act - do whatever the scroll wants to do. This method is called whenever
+     * Act - do whatever the enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- Project game(ADV)/gound.java
+++ Project game(ADV)/gound.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class scroll here.
+ * Write a description of class gound here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class scroll extends Actor
+public class gound extends scroll
 {
     /**
-     * Act - do whatever the scroll wants to do. This method is called whenever
+     * Act - do whatever the gound wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
