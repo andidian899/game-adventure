93c82189edddc2fd94d86d8cf2b3c556e8ba41e5
andidian899 <38680061+andidian899@users.noreply.github.com>
2018-05-13 23:42:49 +0800

    Add files via upload
    tambah bagian akhir tanah

--- Project game(ADV)/gameover.java
+++ Project game(ADV)/gameover.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -17,3 +18,24 @@ public class gameover extends Actor
         // Add your action code here.
     }    
 }
+=======
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class gameover here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class gameover extends Actor
+{
+    /**
+     * Act - do whatever the gameover wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
+>>>>>>> 7f93bfbcde18972cb2198537a9f49f8f5b77297d
